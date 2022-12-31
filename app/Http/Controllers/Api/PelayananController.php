5956f53b32f126804c9f46cd0aa8e9f800cef878
renaamaliafif <renaamalia789@gmail.com>
2023-01-01 00:01:21 +0700

    menambahkan update

--- app/Http/Controllers/Api/PelayananController.php
+++ app/Http/Controllers/Api/PelayananController.php
@@ -34,7 +34,7 @@ class PelayananController extends Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'localhost:8000/api/pelayanan',
+            CURLOPT_URL => 'localhost:8010/api/pelayanan',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
